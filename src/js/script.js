e7bb4061c0a2b29a6440264be247a4dce1726b3f
Marc Whitbread <marcwhitbread@gmail.com>
2015-01-03 01:29:31 -0500

    building animation updated. 3 base stages adde

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,46 @@
 var interval = undefined;
+var floors = 20;
+var basement_floors = 4;
 
 //timing
-var structure_p1_build_delay = 500;
-var structure_p2_build_delay = structure_p1_build_delay * 4;
-var structure_p3_build_delay = structure_p1_build_delay * 8;
+var p1_p2_floor_distance = 4;
+var start_at_floor = 5;
+var structure_p1_build_delay = 2000;
+var p2_build_done_delay = structure_p1_build_delay * 0;
+
+var structure_p2_build_delay = structure_p1_build_delay * p1_p2_floor_distance;
+var structure_crane_build_delay = structure_p1_build_delay * basement_floors;
+var structure_bd1_build_delay = structure_p1_build_delay * 3.5;
+var structure_bd2_sheet_build_delay = structure_p1_build_delay * 8.5;
+var structure_p3_build_delay = structure_p1_build_delay * 1;
+var structure_bd2_build_delay = structure_p1_build_delay * basement_floors - structure_p1_build_delay/2;
+
+/* plot */
+var plot_width = 140;
+var plot_depth = 140;
+var plot_height = 2;
+
+/* crane */
+var crane_width = 8;
+
+/* structure */
+var structure_width = 100;
+var structure_depth = 100;
+var structure_story_indent = 6;
+var structure_story_height = 11;
+var structure_story_railing_height = 3;
+var structure_story_floor_height = 1;
+var structure_story_pillar_indent = 4;
+var structure_roof_height = 5;
+var structure_build_panel_unindent = 2;
+var structure_height = (floors * (structure_story_height + structure_story_floor_height)) + 5;
+var structure_build_deck_height = 5;
+var structure_build_deck_panel_1_height = (structure_story_height + structure_story_floor_height)*1.5;
+var structure_build_deck_panel_2_height = (structure_story_height + structure_story_floor_height)*1.5;
+var structure_wrap_1_height = 30;
+var structure_wrap_2_floors = 5;
+var structure_wrap_2_height = structure_wrap_2_floors * (structure_story_height + structure_story_floor_height);
+var structure_wrap_indent = 5;
 
 $(document).ready(function() {
 	
@@ -14,7 +51,7 @@ $(document).ready(function() {
 function animation(id) {
 	
 	var current_floor = 0;
-	var floors = 26;
+	var delay = 0;
 	
 	interval = setInterval(function() {
 		if(current_floor >= floors) {
@@ -22,61 +59,200 @@ function animation(id) {
 			return true;
 		}
 		
-		buildp1($(id).children('.stories').children('.story').eq(current_floor));
+		build_p1(current_floor, $(id).children('.stories').children('.story').eq(current_floor));
 		
 		current_floor++;
 	}, structure_p1_build_delay);
 	
 }
 
-function buildp1(element) {
+function build_p1(floor, element) {
 	
 	$(element).addClass('p1');
-	
+
 	setTimeout(function() {
-		buildp2(element);
+		build_p2(floor, element);
 	}, structure_p2_build_delay);
 	
+	setTimeout(function() {
+		build_crane(floor, element);
+	}, structure_crane_build_delay);
+	
 }
 
-function buildp2(element) {
+function build_p2(floor, element) {
 	
-	$(element).addClass('p2');
+	$(element).removeClass('p1').addClass('p2');
+	
+	if($(element).hasClass('roof')) {
+		setTimeout(function() {
+			remove_crane();
+		}, p2_build_done_delay);
+	}
+	
+}
+
+function build_crane(floor, element) {
+
+	if(!$('#structure .crane').hasClass('show'))
+		$('#structure .crane').addClass('show')
+			
+	if(floor+1 == floors-basement_floors) {
+		
+		var top = parseInt($(element).css('transform').split(',')[5], 10) - basement_floors * (structure_story_height + structure_story_floor_height) + (structure_story_floor_height + structure_roof_height);
+		
+		$('#structure .crane').css({
+			'-webkit-transform': 'translateX(' + (structure_story_pillar_indent + structure_story_indent) + 'px) translateY(' + top + 'px) translateZ(' + (-structure_width/2 + structure_story_indent + crane_width/2 + structure_story_pillar_indent) + 'px)'
+		});
+
+	} else if(floor < floors-basement_floors) {
+			
+		var top = parseInt($(element).css('transform').split(',')[5], 10) - basement_floors * (structure_story_height + structure_story_floor_height);
+		
+		$('#structure .crane').css({
+			'-webkit-transform': 'translateX(' + (structure_story_pillar_indent + structure_story_indent) + 'px) translateY(' + top + 'px) translateZ(' + (-structure_width/2 + structure_story_indent + crane_width/2 + structure_story_pillar_indent) + 'px)'
+		});
+		
+	}
+	
+	if(floor > floors-5) {
+		
+	}
 	
 	setTimeout(function() {
-		buildp3(element);
-	}, structure_p3_build_delay);
-
+		build_bd1(floor, element);
+	}, structure_bd1_build_delay);
+	
 }
 
-function buildp3(element) {
+function remove_crane() {
 	
-	$(element).addClass('p3');
+	$('#structure .crane').removeClass('show');
 	
-	var top = -7.5 + parseInt($(element).css('transform').split(',')[5], 10);
+}
+
+function build_bd1(floor, element) {
 	
-	console.log(top);
+	if(floor+1 == floors-basement_floors) {
+		
+		var top = parseInt($(element).css('transform').split(',')[5], 10) - basement_floors * (structure_story_height + structure_story_floor_height) + (structure_story_floor_height + structure_roof_height);
+		
+		$('#structure .build-deck-1').css({
+			'-webkit-transform': 'translateY(' + (top + 3) + 'px)'
+		});
+
+	} else if(floor < floors-basement_floors) {
+		if(!$('#structure .build-deck-1').hasClass('show'))
+			$('#structure .build-deck-1').addClass('show')
+			
+		var top = parseInt($(element).css('transform').split(',')[5], 10) - basement_floors * (structure_story_height + structure_story_floor_height);
+		
+		$('#structure .build-deck-1').css({
+			'-webkit-transform': 'translateY(' + (top + 3) + 'px)'
+		});	
+		
+		$('#structure .build-deck-1 .sheet').css({
+			height: (floor * (structure_story_height + structure_story_floor_height) + structure_build_deck_height) + 'px'
+		});
+	}
+		
+	setTimeout(function() {
+		build_bd2_sheet(floor, element);
+	}, structure_bd2_sheet_build_delay);
 	
-	$('#structure .build-deck').css({
-		'-webkit-transform': 'translateY(' + top + 'px)'
-	});
+}
+
+function remove_bd1() {
+	
+	$('#structure .build-deck-1').removeClass('show');
+	
+}
+
+function build_bd2_sheet(floor, element) {
+	
+	if(floor <= structure_wrap_2_floors) {
+		if(!$('#structure .build-deck-2').hasClass('show'))
+			$('#structure .build-deck-2').addClass('show')
+		
+		$('#structure .build-deck-2 .sheet').css({
+			height: (floor * (structure_story_height + structure_story_floor_height)) + 'px'
+		});
+	}
+	
+	setTimeout(function() {
+		build_p3(floor, element);
+	}, structure_p3_build_delay);
+	
+	
+}
+
+function build_p3(floor, element) {
+	
+	$(element).removeClass('p2').addClass('p3');
+	
+	setTimeout(function() {
+		build_bd2(floor, element);
+	}, structure_bd2_build_delay);
+	
+}
+
+function build_bd2(floor, element) {
+	
+	console.log(floor, floors, basement_floors);
+	
+	if(floor+1 == floors-basement_floors) {
+		
+		var top = parseInt($(element).css('transform').split(',')[5], 10) - basement_floors * (structure_story_height + structure_story_floor_height) + (structure_story_floor_height + structure_roof_height);
+		
+		$('#structure .build-deck-2').css({
+			'-webkit-transform': 'translateY(' + (top + 3) + 'px)'
+		});
+
+	} else if(floor < floors-basement_floors) {
+
+		var top = parseInt($(element).css('transform').split(',')[5], 10) - basement_floors * (structure_story_height + structure_story_floor_height);
+		
+		$('#structure .build-deck-2').css({
+			'-webkit-transform': 'translateY(' + (top + 3) + 'px)'
+		});
+		
+	}
+	
+	if(floor+structure_wrap_2_floors+basement_floors+2 >= floors) {
+		
+		$('#structure .build-deck-2 .sheet').css({
+			height: ((floors-floor-structure_wrap_2_floors-2) * (structure_story_height + structure_story_floor_height)) + 'px'
+		});
+		
+	}
+	
+	if($(element).hasClass('roof')) {
+		setTimeout(function() {
+			remove_bd1();
+			remove_bd2();
+		}, p2_build_done_delay + structure_p2_build_delay/2);
+	}
+	
+}
+
+function remove_bd2() {
+	
+	$('#structure .build-deck-2').removeClass('show');
 	
 }
 
 function build(id) {
 	
-	//stories
-	var height = 271;
-	var stories = (height-5) / 11;
-	
-	for(var i = 0; i < 26; i++) {
+	for(var i = 0; i < floors; i++) {
 		
-		var phase = (i > 26-5) ? 'p1' : 
-					(i > 26-6) ? 'p1p2' : 
-					(i > 26-20) ? 'p2' : 'p3';
-		//var phase = (i > 20-8) ? 'p1' : 'p2';
+		var phase = (i > floors-5) ? 'p1' : 
+					(i > floors-6) ? 'p1p2' : 
+					(i > floors-20) ? 'p2' : 'p3';
+		var is_basement = (i < basement_floors) ? ' ug' : (i == basement_floors) ? ' ground' : (i == floors-1) ? ' roof' : '';
+		var p1_built = (i < basement_floors-1) ? ' p1' : '';
+		var p2_built = (i < start_at_floor-basement_floors) ? ' p2' : '';
 		
-		$(id).children('.stories').append($("<div class = 'story " + phase + "'>\
+		$(id).children('.stories').append($("<div class = 'story"+is_basement+p1_built+p2_built+"'>\
 			<div class = 'side'></div>\
 			<div class = 'side'></div>\
 			<div class = 'side'></div>\
@@ -181,20 +357,28 @@ function build(id) {
 		
 	}
 	
-	//roof
-	for(var i = 0; i < 4; i++) {
+	var number_of_tiles = plot_width/((plot_width-structure_width)/2);
+	
+	//base
+	for(var i = 0; i < number_of_tiles; i++) {
+		for(var j = 0; j < number_of_tiles; j++) {
 		
-		$(id).children('.roof').children('.towers').append($("<div class = 'tower'>\
-			<div class = 'side'></div>\
-			<div class = 'side'></div>\
-			<div class = 'side'></div>\
-			<div class = 'side'></div>\
-			<div class = 'side'></div>\
-			<div class = 'side'></div>\
-		</div>"));
+			if((i == 0) || (i == number_of_tiles-1) || (j == 0) || (j == number_of_tiles-1)) {
+				
+				$(id).children('.base-tiles').append($("<div class = 'tile'>\
+					<div class = 'side'></div>\
+					<div class = 'side'></div>\
+					<div class = 'side'></div>\
+					<div class = 'side'></div>\
+					<div class = 'side'></div>\
+					<div class = 'side'></div>\
+				</div>"));
+				
+			}
 		
+		}
 	}
 	
-	//animation(id);
+	animation(id);
 		
 }
