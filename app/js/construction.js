8ae4daffc67ca0f33db85dfa4fa2b47671af8124
Marc Whitbread <marcwhitbread@gmail.com>
2015-01-15 21:19:05 -0500

    Convert sizing to Angular

--- app/js/construction.js
+++ app/js/construction.js
@@ -533,7 +533,7 @@ app.factory('Story', [function() {
 		this.basement_floors = basement_floors;
 		this.structure = structure;
 	
-		this.style = this.set_style('def');
+		this.style = this.set_style(2);
 	}
 	
 	//public methods
@@ -551,57 +551,152 @@ app.factory('Story', [function() {
 			return (this.level == this.floors-1) ? true : false;
 		},
 		
-		set_style: function(style) {
-			switch(style) {
-				case 'def':
-					return {
-						base: {					
-							width: (this.structure.width - this.indent*2) + 'px',
-							height: this.height + this.floor_height + 'px',
-							'-webkit-transform': 'translate3d(' + this.indent + 'px, ' + (-this.level * (this.height + this.floor_height) + (this.structure.basement_floors-1) * (this.height + this.floor_height)) + 'px, 0)'
-						},
-						wall: {
-							width: (this.structure.width - this.indent*2 - this.structure.fence_indent*2) + 'px',
-							height: this.height/2 + 'px',
-							'-webkit-transform': 'translate3d(' + this.structure.fence_indent + 'px, ' + -this.floor_height + 'px, 0)'
-						},
-						floor: {
-							width: (this.structure.width - this.indent*2) + 'px',
-							height: this.floor_height + 'px',
-							'-webkit-transform': 'translate3d(0, 0, 0)'
-						},
-						girder: [{
-							width: (this.structure.width - this.indent*2 - this.structure.story_pillar_indent*2 - this.structure.pillar_p2_width + this.structure.steel_width) + 'px',
-							height: this.floor_height + 'px',
-							'-webkit-transform': 'translate3d(' + (this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, 0, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.pillar_indent) + 'px)'
-						}, {
-							width: (this.structure.width - this.indent*2 - this.structure.story_pillar_indent*2 - this.structure.pillar_p2_width + this.structure.steel_width) + 'px',
-							height: this.floor_height + 'px',
-							'-webkit-transform': 'translate3d(' + (this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, 0, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.pillar_indent) + 'px)'
-						}, {
-							width: this.structure.steel_width + 'px',
-							height: this.floor_height + 'px',
-							'-webkit-transform': 'translate3d(' + this.indent + 'px, 0, 0)'
-						}, {
-							width: this.structure.steel_width + 'px',
-							height: this.floor_height + 'px',
-							'-webkit-transform': 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width/2 - this.pillar_indent - this.structure.steel_width/2) + 'px, 0, 0)'
-						}],
-						pillar: [{
-							width: this.structure.steel_width + 'px',
-							height: '0px'
-						},{
-							width: this.structure.steel_width + 'px',
-							height: '0px'
-						},{
-							width: this.structure.steel_width + 'px',
-							height: '0px'
-						},{
-							width: this.structure.steel_width + 'px',
-							height: '0px'
-						}]
-					}
+		set_style: function(level) {
+
+			var style = {
+				base: {					
+					width: (this.structure.width - this.indent*2) + 'px',
+					height: this.height + this.floor_height + 'px',
+					'-webkit-transform': 'translate3d(' + this.indent + 'px, ' + (-this.level * (this.height + this.floor_height) + (this.structure.basement_floors-1) * (this.height + this.floor_height)) + 'px, 0)'
+				},
+				wall: {
+					width: (this.structure.width - this.indent*2 - this.structure.fence_indent*2) + 'px',
+					height: this.height/2 + 'px',
+					'-webkit-transform': 'translate3d(' + this.structure.fence_indent + 'px, ' + -this.floor_height + 'px, 0)'
+				},
+				floor: {
+					width: (this.structure.width - this.indent*2) + 'px',
+					height: this.floor_height + 'px',
+					'-webkit-transform': 'translate3d(0, 0, 0)'
+				},
+				girder: [{
+					width: (this.structure.width - this.indent*2 - this.structure.story_pillar_indent*2 - this.structure.pillar_p2_width + this.structure.steel_width) + 'px',
+					height: this.floor_height + 'px',
+					'-webkit-transform': 'translate3d(' + (this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, 0, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.pillar_indent) + 'px)'
+				}, {
+					width: (this.structure.width - this.indent*2 - this.structure.story_pillar_indent*2 - this.structure.pillar_p2_width + this.structure.steel_width) + 'px',
+					height: this.floor_height + 'px',
+					'-webkit-transform': 'translate3d(' + (this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, 0, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.pillar_indent) + 'px)'
+				}, {
+					width: this.structure.steel_width + 'px',
+					height: this.floor_height + 'px',
+					'-webkit-transform': 'translate3d(' + this.indent + 'px, 0, 0)'
+				}, {
+					width: this.structure.steel_width + 'px',
+					height: this.floor_height + 'px',
+					'-webkit-transform': 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width/2 - this.pillar_indent - this.structure.steel_width/2) + 'px, 0, 0)'
+				}],
+				pillar: [{
+					width: this.structure.steel_width + 'px',
+					height: '0',
+					'-webkit-transform': 'translate3d(' + (this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, ' + -this.floor_height + 'px, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.structure.story_pillar_indent) + 'px)'
+				},{
+					width: this.structure.steel_width + 'px',
+					height: '0',
+					'-webkit-transform': 'translate3d(' + (this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, ' + -this.floor_height + 'px, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.structure.story_pillar_indent) + 'px)'
+				},{
+					width: this.structure.steel_width + 'px',
+					height: '0',
+					'-webkit-transform': 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width - this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, ' + -this.floor_height + 'px, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.pillar_indent) + 'px)'
+				},{
+					width: this.structure.steel_width + 'px',
+					height: '0',
+					'-webkit-transform': 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width - this.pillar_indent + (this.structure.pillar_p2_width - this.structure.steel_width)/2) + 'px, ' + -this.floor_height + 'px, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.pillar_indent) + 'px)'
+				}]
 			}
+			
+			if(this.is_underground() || this.is_ground()) {
+				style.floor.width = (this.structure.width - this.indent) + 'px';
+				style.floor.webkitTransform = 'translate3d(' + -this.indent/2 + 'px, 0, 0)';
+			}
+			
+			if(this.is_underground()) {
+				style.wall.width = (this.structure.width - this.indent/2) + 'px';
+				style.wall.webkitTransform = 'translate3d(' + -this.indent/2 + 'px, ' + -this.floor_height + 'px, 0)'
+			}
+			
+			if(level == 1) {
+				style.pillar[0].height = this.height + 'px';
+				style.pillar[1].height = this.height + 'px';
+				style.pillar[2].height = this.height + 'px';
+				style.pillar[3].height = this.height + 'px';
+				
+				if(this.is_roof()) {
+					style.pillar[0].height = (this.structure.roof_height - this.floor_height) + 'px';
+					style.pillar[1].height = (this.structure.roof_height - this.floor_height) + 'px';
+					style.pillar[2].height = (this.structure.roof_height - this.floor_height) + 'px';
+					style.pillar[3].height = (this.structure.roof_height - this.floor_height) + 'px';
+				}
+			}
+			
+			if(level == 2) {
+				style.pillar[0].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[0].height = this.height + 'px';
+				style.pillar[0].webkitTransform = 'translate3d(' + this.pillar_indent + 'px, ' + -this.floor_height + 'px, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.pillar_indent) + 'px)';
+				
+				style.pillar[1].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[1].height = this.height + 'px';
+				style.pillar[1].webkitTransform = 'translate3d(' + this.pillar_indent + 'px, ' + -this.floor_height + 'px, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.pillar_indent) + 'px)';
+				
+				style.pillar[2].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[2].height = this.height + 'px';
+				style.pillar[2].webkitTransform = 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width - this.pillar_indent) + 'px, ' + -this.floor_height + 'px, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.pillar_indent) + 'px)';
+				
+				style.pillar[3].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[3].height = this.height + 'px';
+				style.pillar[3].webkitTransform = 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width - this.pillar_indent) + 'px, ' + -this.floor_height + 'px, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.pillar_indent) + 'px)';
+				
+				if(this.is_roof()) {
+					style.wall.width = (this.structure.width - this.indent*2) + 'px';
+					style.wall.height = this.roof_height - this.floor_height + 'px';
+					style.wall.webkitTransform = 'translate3d(0, ' + -this.floor_height + 'px, 0)';
+					
+					style.pillar[0].height = this.roof_height - this.floor_height + 'px';
+					style.pillar[1].height = this.roof_height - this.floor_height + 'px';
+					style.pillar[2].height = this.roof_height - this.floor_height + 'px';
+					style.pillar[3].height = this.roof_height - this.floor_height + 'px';
+				}
+			}
+			
+			if(level == 3) {
+				style.wall.width = (this.structure.width - this.indent*2) + 'px';
+				style.wall.height = this.height + 'px';
+				style.wall.webkitTransform = 'translate3d(0, ' + -this.floor_height + 'px, 0)';
+				
+				style.floor.width = (this.structure.width - this.indent*2) + 'px';
+				style.floor.height = this.floor_height + 'px';
+				
+				style.pillar[0].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[0].webkitTransform = 'translate3d(' + this.pillar_indent + 'px, ' + -this.floor_height + 'px, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.pillar_indent) + 'px)';
+				
+				style.pillar[1].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[1].webkitTransform = 'translate3d(' + this.pillar_indent + 'px, ' + -this.floor_height + 'px, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.pillar_indent) + 'px)';
+				
+				style.pillar[2].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[2].webkitTransform = 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width - this.pillar_indent) + 'px, ' + -this.floor_height + 'px, ' + (-this.structure.width/2 + this.indent + this.structure.pillar_p2_width/2 + this.pillar_indent) + 'px)';
+				
+				style.pillar[3].width = this.structure.pillar_p2_width + 'px';
+				style.pillar[3].webkitTransform = 'translate3d(' + (this.structure.width - this.indent*2 - this.structure.pillar_p2_width - this.pillar_indent) + 'px, ' + -this.floor_height + 'px, ' + (this.structure.width/2 - this.indent - this.structure.pillar_p2_width/2 - this.pillar_indent) + 'px)';
+				
+				if(this.is_roof()) {
+					style.wall.height = this.roof_height - this.floor_height + 'px';	
+				}
+				
+				if(this.is_underground() || this.is_ground()) {
+					style.floor.width = this.structure.width - this.indent + 'px';
+					style.floor.webkitTransform = 'translate3d(' + -this.indent/2 + 'px, 0, 0)';
+				}
+				
+				if(this.is_underground()) {
+					style.wall.width = this.structure.width - this.indent + 'px';
+					style.wall.height = this.height/2 + 'px';
+					style.wall.webkitTransform = 'translate3d(' + -this.indent/2 + 'px, ' + -this.floor_height + 'px, 0)';
+				}
+			}
+			
+			console.log(style.pillar[0]);
+			
+			return style;
 		}
 		
 	}
@@ -609,10 +704,6 @@ app.factory('Story', [function() {
 	return Story;
 	
 }]);
-
-/*
-
-*/
 app.factory('Structure', [function() {
 	
 	//constructor
